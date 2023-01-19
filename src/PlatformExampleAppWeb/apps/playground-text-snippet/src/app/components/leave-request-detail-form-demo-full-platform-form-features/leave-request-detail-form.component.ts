c5c09d95dab00ee096ca51c32271dbca0db2f9bf
Duc Dang <duc.dang@orientsoftware.com>
2023-01-19 13:35:57 +0700

    Update example app code for form

--- src/PlatformExampleAppWeb/apps/playground-text-snippet/src/app/components/leave-request-detail-form-demo-full-platform-form-features/leave-request-detail-form.component.ts
+++ src/PlatformExampleAppWeb/apps/playground-text-snippet/src/app/components/leave-request-detail-form-demo-full-platform-form-features/leave-request-detail-form.component.ts
@@ -1,22 +1,56 @@
 // import { CommonModule } from '@angular/common';
-// import { ChangeDetectionStrategy, Component, Input, OnInit, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
+// import { ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 // import { FormControl, Validators } from '@angular/forms';
-// import { DateAdapter } from '@angular/material/core';
 // import { MatDatepickerModule } from '@angular/material/datepicker';
 // import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+// import { MatLegacyDialogRef } from '@angular/material/legacy-dialog';
 // import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 // import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 // import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
-// import { FormHelpers, ifAsyncValidator, PlatformFormComponent, PlatformFormConfig, startEndValidator } from '@libs/platform-core';
-// import { combineLatest, map, Observable, of } from 'rxjs';
+// import { BravoCommonModule, BravoDialogService, BravoSelectDefaultItem } from '@orient/bravo-common';
+// import {
+//   checkIsLeaveRequestDateRangeOverlappedAsyncValidator,
+//   LeaveRequest,
+//   LeaveRequestApiService,
+//   LeaveRequestDateType,
+//   LeaveRequestStatusComponent,
+//   LeaveType,
+//   LeaveTypeApiService,
+//   OrgApiService,
+//   RemainingLeave,
+//   RemainingLeaveApiService
+// } from '@orient/bravo-domain/growth';
+// import {
+//   buildFormValidationErrors,
+//   FormHelpers,
+//   ifAsyncValidator,
+//   noWhitespaceValidator,
+//   PlatformApiServiceErrorResponse,
+//   PlatformFormComponent,
+//   PlatformFormConfig,
+//   startEndValidator
+// } from '@orient/platform-core';
+// import { catchError, combineLatest, delay, map, Observable, of, switchMap, tap } from 'rxjs';
 
-// import { checkIsLeaveRequestDateRangeOverlappedAsyncValidator } from '../../form-validators';
-// import { LeaveRequestDetailFormDemoFullFormFeatureVm } from './leave-request-detail-form.view-model';
+// import { AuthService } from '../../../core/auth/auth.service';
+// import { LeaveTypeDisplayPipe } from '../../pipes/leave-type-display.pipe';
+// import { CancelLeaveRequestComponent } from '../cancel-leave-request/cancel-leave-request.component';
+// import { ConfirmationComponent } from '../confirmation/confirmation.component';
+// import { EmployeeCardComponent } from '../employee-info/employee-card.component';
+// import { LeaveRequestApproverComponent } from '../leave-request-approver/leave-request-approver.component';
+// import { LeaveRequestDetailFormVm } from './leave-request-detail-form.view-model';
+
+// export enum LeaveRequestDetailFormValidationKeys {
+//   requestDateRangeOverlapped = 'requestDateRangeOverlapped',
+//   notEnoughRemainingLeave = 'notEnoughRemainingLeave',
+//   reachedMaximumTotalDays = 'reachedMaximumTotalDays'
+// }
 
 // @Component({
 //   standalone: true,
 //   imports: [
 //     CommonModule,
+//     BravoCommonModule,
 //     MatButtonModule,
 //     MatDatepickerModule,
 //     MatRadioModule,
@@ -26,52 +60,45 @@
 //     MatFormFieldModule,
 //     MatInputModule,
 //     LeaveTypeDisplayPipe,
-//     CancelLeaveRequestComponent
+//     EmployeeCardComponent
 //   ],
-//   selector: 'app-leave-request-detail-form-demo-full-features [organization]',
+//   selector: 'app-leave-request-detail-form [organization]',
 //   templateUrl: './leave-request-detail-form.component.html',
 //   styleUrls: ['./leave-request-detail-form.component.scss'],
 //   encapsulation: ViewEncapsulation.None,
-//   changeDetection: ChangeDetectionStrategy.OnPush,
-//   providers: [{ provide: DateAdapter, useClass: CustomDateAdapter }]
+//   changeDetection: ChangeDetectionStrategy.OnPush
 // })
-// export class LeaveRequestDetailDemoFullFeaturesFormComponent
-//   extends PlatformFormComponent<LeaveRequestDetailFormDemoFullFormFeatureVm>
-//   implements OnInit
-// {
+// export class LeaveRequestDetailFormComponent extends PlatformFormComponent<LeaveRequestDetailFormVm> implements OnInit {
 //   @Input() public selectedCompanyId!: string;
 //   @Input() public leaveRequest?: LeaveRequest;
 //   @Input() public leaveRequestId?: string;
 
-//   @ViewChildren(CancelLeaveRequestComponent) public cancelLeaveRequestForms: QueryList<CancelLeaveRequestComponent>;
-
-//   public leaveTypeSelectOptions: BravoSelectDefaultItem<string>[] = [
-//     { value: LeaveRequestType.AnnualLeave, label: LeaveRequestType.AnnualLeave },
-//     { value: LeaveRequestType.UnpaidLeave, label: LeaveRequestType.UnpaidLeave },
-//     { value: LeaveRequestType.SickLeave, label: LeaveRequestType.SickLeave },
-//     { value: LeaveRequestType.MarriageLeave, label: LeaveRequestType.MarriageLeave },
-//     { value: LeaveRequestType.ChildrenMarriageLeave, label: LeaveRequestType.ChildrenMarriageLeave },
-//     { value: LeaveRequestType.BereavementLeave, label: LeaveRequestType.BereavementLeave }
-//   ];
+//   public leaveTypeSelectOptions: BravoSelectDefaultItem<string>[] = [];
+//   public availableLeaveTypes: LeaveType[] = [];
 //   public weekday: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//   public approver: LeaveRequestApprover;
+//   public dateTypeDisplay: Record<LeaveRequestDateType, string> = LeaveRequest.dateTypeDisplay;
+//   public remainingLeave?: RemainingLeave;
 
 //   constructor(
 //     private bravoDialogService: BravoDialogService,
 //     private leaveRequestApi: LeaveRequestApiService,
-//     private leaveTypeApi: LeaveTypeApiService,
 //     private orgApiService: OrgApiService,
-//     private authService: AuthService
+//     private leaveTypeApi: LeaveTypeApiService,
+//     private authService: AuthService,
+//     private dialogRef: MatLegacyDialogRef<LeaveRequestDetailFormComponent>,
+//     private remainingLeaveApi: RemainingLeaveApiService
 //   ) {
 //     super();
 //   }
 
-//   protected onInitVm = (): Observable<LeaveRequestDetailFormDemoFullFormFeatureVm> => {
+//   protected onInitVm = (): Observable<LeaveRequestDetailFormVm> => {
 //     return combineLatest([
 //       this.leaveRequestApi.getLeaveRequestApprover({
 //         companyId: this.selectedCompanyId
 //       }),
-//       this.leaveRequestId != null ? this.leaveRequestApi.getLeaveRequestDetail(this.leaveRequestId) : of(this.leaveRequest),
+//       this.leaveRequestId != null
+//         ? this.leaveRequestApi.getLeaveRequestDetail(this.leaveRequestId)
+//         : of(this.leaveRequest),
 //       this.orgApiService.getOrg(this.selectedCompanyId),
 //       this.leaveTypeApi.getLeaveType({
 //         skipCount: 0,
@@ -79,27 +106,33 @@
 //       })
 //     ]).pipe(
 //       this.observerLoadingState(),
-//       map(([approver, leaveRequest, selectedCompany, leaveTypes]) => {
-//         console.log(leaveTypes, 1);
-//         const initialVm = new LeaveRequestDetailFormDemoFullFormFeatureVm({
+//       map(([approver, leaveRequest, selectedCompany, leaveTypesResult]) => {
+//         this.availableLeaveTypes = leaveTypesResult.items ?? [];
+//         this.leaveTypeSelectOptions = leaveTypesResult.items
+//           .map(item => ({
+//             value: item.id,
+//             label: item.name
+//           }))
+//           .sort((previous, next) => previous.label.localeCompare(next.label));
+
+//         const initialVm: LeaveRequestDetailFormVm = new LeaveRequestDetailFormVm({
 //           mode: this.mode,
 //           approver: approver,
 //           leaveRequest: leaveRequest,
 //           companyId: this.selectedCompanyId,
-//           selectedCompany: selectedCompany
+//           selectedCompany: selectedCompany,
+//           leaveTypeId: this.leaveTypeSelectOptions[0].value
 //         });
-
-//         initialVm.formData.employeeId = this.authService.currentUserInfo.userId;
 
 //         return initialVm;
 //       })
 //     );
 //   };
 
-//   protected initialFormConfig = (): PlatformFormConfig<LeaveRequestDetailFormDemoFullFormFeatureVm> | undefined => {
+//   protected initialFormConfig = (): PlatformFormConfig<LeaveRequestDetailFormVm> => {
 //     return {
 //       controls: {
-//         reason: new FormControl(this.vm.reason, Validators.required),
+//         reason: new FormControl(this.vm.reason, [Validators.required, noWhitespaceValidator]),
 //         fromDate: new FormControl(
 //           this.vm.fromDate,
 //           [
@@ -115,14 +148,14 @@
 //             ifAsyncValidator(
 //               () => !this.isViewMode,
 //               checkIsLeaveRequestDateRangeOverlappedAsyncValidator(
-//                 'requestDateRangeOverlapped',
+//                 LeaveRequestDetailFormValidationKeys.requestDateRangeOverlapped,
 //                 query => this.leaveRequestApi.checkOverlapLeaveRequest(query).pipe(this.finalDetectChanges()),
 //                 () => ({
 //                   fromDate: this.vm.getFromDateBaseOnMorningWorkingHour(),
 //                   toDate: this.vm.getToDateBaseOnAfternoonWorkingHour(),
-//                   userId: this.vm.formData.employeeId,
+//                   userId: this.authService.currentUserInfo.userId,
 //                   checkForCurrentExistingRequestId: this.vm.formData.id,
-//                   companyId: this.selectedCompanyId
+//                   companyId: this.vm.selectedCompany.id
 //                 })
 //               )
 //             )
@@ -130,29 +163,59 @@
 //         ),
 //         toDate: new FormControl(this.vm.toDate, [Validators.required]),
 //         type: new FormControl(this.vm.type, Validators.required),
+//         leaveTypeId: new FormControl(this.vm.leaveTypeId, Validators.required, [
+//           ifAsyncValidator(
+//             control => !this.isViewMode && this.vm.canValidateRemainingDate(control.value, this.availableLeaveTypes),
+//             control => {
+//               return of(control.value).pipe(
+//                 delay(100),
+//                 switchMap(() => this.remainingLeaveApi.getRemainingLeave(control.value)),
+//                 tap(result => {
+//                   this.remainingLeave = result;
+//                   this.detectChanges();
+//                 }),
+//                 map((remainingLeave: RemainingLeave) => {
+//                   if (remainingLeave.currentRemainingInfo.remainingDays <= 0) {
+//                     return buildFormValidationErrors(
+//                       LeaveRequestDetailFormValidationKeys.notEnoughRemainingLeave,
+//                       'The number of remaining leaves is not sufficient for this leave type. Please try another one!'
+//                     );
+//                   } else if (remainingLeave.currentRemainingInfo.remainingDays < this.vm.totalDays) {
+//                     return buildFormValidationErrors(
+//                       LeaveRequestDetailFormValidationKeys.reachedMaximumTotalDays,
+//                       'The total number of leaves could not be greater than the remaining leaves.'
+//                     );
+//                   }
+
+//                   return null;
+//                 }),
+//                 catchError((error: PlatformApiServiceErrorResponse | Error) =>
+//                   of(
+//                     buildFormValidationErrors(
+//                       LeaveRequestDetailFormValidationKeys.notEnoughRemainingLeave,
+//                       PlatformApiServiceErrorResponse.getDefaultFormattedMessage(error)
+//                     )
+//                   )
+//                 )
+//               );
+//             }
+//           )
+//         ]),
 //         companyId: new FormControl(this.vm.companyId, Validators.required),
 //         totalDays: new FormControl(this.vm.totalDays, Validators.required),
-//         testFormArrayGroupLeaveRequests: {
-//           modelItems: () => this.vm.testFormArrayGroupLeaveRequests,
-//           itemControl: item => ({
-//             reason: new FormControl(item.reason, Validators.required),
-//             leaveRequestId: new FormControl(item.leaveRequestId, Validators.required)
-//           })
-//         },
-//         testPrimitivePropsForArrayControls: {
-//           modelItems: () => this.vm.testPrimitivePropsForArrayControls,
-//           itemControl: item => new FormControl(item, [Validators.required])
-//         }
+//         approverId: new FormControl(this.vm.approverId, Validators.required)
 //       },
-//       groupValidations: [['fromDate', 'toDate']],
-//       childForms: () => [this.cancelLeaveRequestForms]
+//       groupValidations: [
+//         ['fromDate', 'toDate'],
+//         ['totalDays', 'leaveTypeId']
+//       ]
 //     };
 //   };
 
-//   public notWeekendDatesFilter = LeaveRequestDetailFormDemoFullFormFeatureVm.notWeekendDatesFilter;
+//   public notWeekendDatesFilter = LeaveRequest.notWeekendDatesFilter;
 
 //   public close() {
-//     this.bravoDialogService.closeLastCurrentDialog();
+//     this.dialogRef.close();
 //   }
 
 //   public onLeaveRequestDateTypeChanged(): void {
@@ -173,6 +236,7 @@
 
 //   public submitLeaveRequest = this.effect(() => {
 //     this.form.disable();
+//     console.log(this.vm.formData);
 //     return this.leaveRequestApi
 //       .createLeaveRequest({
 //         data: {
@@ -185,7 +249,7 @@
 //         this.observerLoadingState('submitLeaveRequest'),
 //         this.tapResponse(
 //           () => {
-//             this.toast.success('Successfully create your leave request!');
+//             this.toast.success('Your leave request has been created successfully');
 //             this.bravoDialogService.closeLastCurrentDialog({ saveLeaveRequest: true });
 //           },
 //           () => {
@@ -195,7 +259,7 @@
 //       );
 //   });
 
-//   public cancelLeaveRequest(): void {
+//   public closeDialog(): void {
 //     this.bravoDialogService
 //       .openConfirmDialog({
 //         confirmMsg: 'Your request will be discarded. Do you want to continue?',
@@ -208,7 +272,23 @@
 //       });
 //   }
 
-//   public testAddMoreItemForArrayFormDemo() {
-//     this.updateVm(p => p.testAddMoreItemForArrayFormDemo());
+//   public onCancelCreatedLeaveRequest() {
+//     if (!this.vm.leaveRequest) return;
+
+//     this.bravoDialogService
+//       .openDialog(
+//         CancelLeaveRequestComponent,
+//         {
+//           leaveRequestId: this.vm.leaveRequest.id,
+//           currentLeaveRequestStatus: this.vm.leaveRequest.status
+//         },
+//         {
+//           autoFocus: false,
+//           closeOn$: this.destroyed$
+//         }
+//       )
+//       .subscribe(reloadRequired => {
+//         if (reloadRequired) this.dialogRef.close({ saveLeaveRequest: reloadRequired });
+//       });
 //   }
 // }
--- src/PlatformExampleAppWeb/apps/playground-text-snippet/src/app/form-validators/checkIsLeaveRequestDateRangeOverlappedAsyncValidator.ts
+++ src/PlatformExampleAppWeb/apps/playground-text-snippet/src/app/form-validators/checkIsLeaveRequestDateRangeOverlappedAsyncValidator.ts
@@ -1,40 +1,40 @@
-import { AsyncValidatorFn, FormControl, ValidationErrors } from '@angular/forms';
-import { buildFormValidationError, PlatformApiServiceErrorResponse } from '@orient/platform-core';
-import { catchError, delay, map, Observable, of, Subject, switchMap, takeUntil } from 'rxjs';
+// import { AsyncValidatorFn, FormControl, ValidationErrors } from '@angular/forms';
+// import { buildFormValidationErrors, PlatformApiServiceErrorResponse } from '@orient/platform-core';
+// import { catchError, delay, map, Observable, of, Subject, switchMap, takeUntil } from 'rxjs';
 
-import { CheckOverlapLeaveRequestQuery, CheckOverlapLeaveRequestQueryResult } from '../api-services';
+// import { CheckOverlapLeaveRequestQuery, CheckOverlapLeaveRequestQueryResult } from '../api-services';
 
-export function checkIsLeaveRequestDateRangeOverlappedAsyncValidator(
-  errorKey: string,
-  checkOverlap$: (query: CheckOverlapLeaveRequestQuery) => Observable<CheckOverlapLeaveRequestQueryResult>,
-  queryFn: (control: FormControl<Date>) => CheckOverlapLeaveRequestQuery | null
-): AsyncValidatorFn {
-  const cancelPreviousSub: Subject<null> = new Subject();
+// export function checkIsLeaveRequestDateRangeOverlappedAsyncValidator(
+//   errorKey: string,
+//   checkOverlap$: (query: CheckOverlapLeaveRequestQuery) => Observable<CheckOverlapLeaveRequestQueryResult>,
+//   queryFn: (control: FormControl<Date>) => CheckOverlapLeaveRequestQuery | null
+// ): AsyncValidatorFn {
+//   const cancelPreviousSub: Subject<null> = new Subject();
 
-  return (control: FormControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
-    cancelPreviousSub.next(null);
-    const query = queryFn(control);
+//   return (control: FormControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
+//     cancelPreviousSub.next(null);
+//     const query = queryFn(control);
 
-    if (query == null) {
-      return of(null);
-    }
+//     if (query == null) {
+//       return of(null);
+//     }
 
-    return of(null).pipe(
-      takeUntil(cancelPreviousSub),
-      delay(300),
-      switchMap(() => {
-        return checkOverlap$(query).pipe(
-          takeUntil(cancelPreviousSub),
-          map((result: CheckOverlapLeaveRequestQueryResult) => {
-            return result.isOverlapped
-              ? { [errorKey]: buildFormValidationError({ errorMsg: 'The given date range is overlapping with the previous requests' }) }
-              : null;
-          }),
-          catchError((error: PlatformApiServiceErrorResponse | Error) =>
-            of({ [errorKey]: buildFormValidationError({ errorMsg: PlatformApiServiceErrorResponse.getDefaultFormattedMessage(error) }) })
-          )
-        );
-      })
-    );
-  };
-}
+//     return of(null).pipe(
+//       takeUntil(cancelPreviousSub),
+//       delay(300),
+//       switchMap(() => {
+//         return checkOverlap$(query).pipe(
+//           takeUntil(cancelPreviousSub),
+//           map((result: CheckOverlapLeaveRequestQueryResult) => {
+//             return result.isOverlapped
+//               ? buildFormValidationErrors(errorKey, 'The given date range is overlapping with the previous requests')
+//               : null;
+//           }),
+//           catchError((error: PlatformApiServiceErrorResponse | Error) =>
+//             of(buildFormValidationErrors(errorKey, PlatformApiServiceErrorResponse.getDefaultFormattedMessage(error)))
+//           )
+//         );
+//       })
+//     );
+//   };
+// }
