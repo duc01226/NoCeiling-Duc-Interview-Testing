8d1c01f9146b760ca0ff9bbe7f531df39fc1b2ff
Duc Dang <duc.dang@orientsoftware.com>
2023-04-22 08:58:03 +0700

    Fix bug slow query empty list

--- src/Platform/Easy.Platform/Application/Persistence/IPlatformDbContext.cs
+++ src/Platform/Easy.Platform/Application/Persistence/IPlatformDbContext.cs
@@ -29,6 +29,7 @@ public interface IPlatformDbContext : IDisposable
         var queryElapsedTime = Stopwatch.GetElapsedTime(startQueryTimeStamp);
 
         if (result?.GetType().IsAssignableToGenericType(typeof(IEnumerable<>)) == true &&
+            result.As<IEnumerable<object>>()?.Any() == true &&
             result.As<IEnumerable<object>>()?.Count() >=
             persistenceConfiguration.GetBadQueryWarningTotalItemsThreshold(result.As<IEnumerable<object>>().First().GetType()))
             LogTooMuchDataInMemoryBadQueryWarning(result.As<IEnumerable<object>>(), logger, persistenceConfiguration);
