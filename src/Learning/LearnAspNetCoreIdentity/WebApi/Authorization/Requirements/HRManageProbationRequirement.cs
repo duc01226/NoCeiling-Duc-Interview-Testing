8e74c9c8d84bcf74afa3bae59cae1526b99af408
Duc Dang <duc.dang@orientsoftware.com>
2023-05-26 10:02:47 +0700

    Refactor learning

--- src/Learning/LearnAspNetCoreIdentity/WebApi/Authorization/Requirements/HRManageProbationRequirement.cs
+++ src/Learning/LearnAspNetCoreIdentity/WebApi/Authorization/Requirements/HRManageProbationRequirement.cs
@@ -3,40 +3,18 @@ using Microsoft.AspNetCore.Authorization;
 using WebApi.Auth;
 using WebApi.Common.Extensions;
 
-namespace WebApi.Authorization.Requirements
+namespace WebApi.Authorization.Requirements;
+
+public class HrManageProbationRequirement : IAuthorizationRequirement
 {
-    public class HrManageProbationRequirement : IAuthorizationRequirement
+    public const int DefaultMinimumProbationMonths = 3;
+
+    public HrManageProbationRequirement(int probationMonths)
     {
-        public const int DefaultMinimumProbationMonths = 3;
-
-        public HrManageProbationRequirement(int probationMonths)
-        {
-            ProbationMonths = probationMonths;
-        }
-
-        public int ProbationMonths { get; set; }
-
-        public int ProbationDays => ProbationMonths * 30;
+        ProbationMonths = probationMonths;
     }
 
-    public class HrManageProbationRequirementHandler : AuthorizationHandler<HrManageProbationRequirement>
-    {
-        protected override Task HandleRequirementAsync(
-            AuthorizationHandlerContext context,
-            HrManageProbationRequirement requirement)
-        {
-            var employmentDate = context.User.FindFirstValue(AppAuthenticationClaims.EmploymentDate.Type)
-                ?.TryParseDateTime();
+    public int ProbationMonths { get; set; }
 
-            if (employmentDate != null)
-            {
-                var probationPeriod = DateTime.UtcNow - employmentDate.Value;
-
-                if (probationPeriod.Days > requirement.ProbationDays)
-                    context.Succeed(requirement);
-            }
-
-            return Task.CompletedTask;
-        }
-    }
+    public int ProbationDays => ProbationMonths * 30;
 }
