9b319c326a1ad51ed463cc412b76b1a8707ae6b2
Duc Dang <duc.dang@orientsoftware.com>
2024-06-06 13:50:34 +0700

    Update

--- src/PlatformExampleAppWeb/libs/platform-core/src/lib/directives/abstracts/platform.directive.ts
+++ src/PlatformExampleAppWeb/libs/platform-core/src/lib/directives/abstracts/platform.directive.ts
@@ -1,68 +1,12 @@
-import { AfterViewInit, ChangeDetectorRef, Directive, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject, MonoTypeOperatorFunction, Subscription } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
+import { ElementRef } from '@angular/core';
+import { PlatformComponent } from '../../components';
 
-import { task_delay } from '../../utils';
-
-@Directive()
-export abstract class PlatformDirective implements OnInit, AfterViewInit, OnDestroy {
-    public static get defaultDetectChangesDelay(): number {
-        return 100;
+export class PlatformDirective extends PlatformComponent {
+    constructor(public elementRef: ElementRef) {
+        super();
     }
 
-    public constructor(protected changeDetector: ChangeDetectorRef) {}
-
-    public initiated$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public viewInitiated$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public destroyed$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
-    private detectChangesDelaySubs: Subscription = new Subscription();
-
-    public detectChanges(delayTime?: number, onDone?: () => unknown, checkParentForHostBinding: boolean = false): void {
-        this.detectChangesDelaySubs.unsubscribe();
-        if (!this.canDetectChanges) {
-            return;
-        }
-
-        const finalDelayTime = delayTime == null ? PlatformDirective.defaultDetectChangesDelay : delayTime;
-        this.detectChangesDelaySubs = task_delay(() => {
-            if (this.canDetectChanges) {
-                this.changeDetector.detectChanges();
-                if (checkParentForHostBinding) {
-                    this.changeDetector.markForCheck();
-                }
-                if (onDone != null) {
-                    onDone();
-                }
-            }
-        }, finalDelayTime);
-    }
-
-    public untilDestroyed<T>(): MonoTypeOperatorFunction<T> {
-        return takeUntil(this.destroyed$.pipe(filter(destroyed => destroyed == true)));
-    }
-
-    public ngOnInit(): void {
-        this.initiated$.next(true);
-    }
-
-    public ngAfterViewInit(): void {
-        this.viewInitiated$.next(true);
-    }
-
-    public ngOnDestroy(): void {
-        this.destroyed$.next(true);
-
-        this.destroyAllSubjects();
-    }
-
-    protected get canDetectChanges(): boolean {
-        return this.initiated$.value && !this.destroyed$.value;
-    }
-
-    private destroyAllSubjects(): void {
-        this.initiated$.complete();
-        this.viewInitiated$.complete();
-        this.destroyed$.complete();
+    public get element(): HTMLElement {
+        return this.elementRef.nativeElement;
     }
 }
